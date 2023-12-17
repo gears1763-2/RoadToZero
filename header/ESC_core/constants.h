a65da912878ad73e99b09658a1c44ba32e723529
gears1763-2 <91225514+gears1763-2@users.noreply.github.com>
2023-12-17 00:29:16 -0800

    Add files via upload

--- header/ESC_core/constants.h
+++ header/ESC_core/constants.h
@@ -22,19 +22,28 @@
 #include "includes.h"
 
 
-//  1. testing constants
+// ======== TESTING CONSTANTS ======================================================= //
+
 const double FLOAT_TOLERANCE = 1e-6; ///< Tolerance for floating point equality tests.
 
+// ======== END TESTING CONSTANTS =================================================== //
 
-//  2. general game constants
+
+
+// ======== GENERAL GAME CONSTANTS ================================================== //
+
 const int FRAMES_PER_SECOND = 60; ///< Target frames per second.
 const double SECONDS_PER_FRAME = 1.0 / 60; ///< Target seconds per frame (just reciprocal of target frames per second).
 
 const int GAME_WIDTH = 1200; ///< Width of the game space.
 const int GAME_HEIGHT = 800; ///< Height of the game space.
 
+// ======== END GENERAL GAME CONSTANTS ============================================== //
 
-//  3. HexTile and HexMap constants
+
+
+// ======== HEXTILE AND HEXMAP CONSTANTS ============================================ //
+
 const sf::Color FOREST_GREEN(34, 139, 34); ///< The base colour of a forest tile.
 const sf::Color LAKE_BLUE(0, 102, 204); ///< The base colour of a lake (water) tile.
 const sf::Color MOUNTAINS_GREY(97, 110, 113); ///< The base colour of a mountains tile.
@@ -46,7 +55,7 @@ const std::vector<double> tile_type_cumulative_probabilities = {
     0.50,  // PLAINS
     0.75,  // FOREST
     1.00   // MOUNTAINS
-};
+}; ///< Cumulative probabilities for each tile type (to support procedural generation).
 
 const std::vector<double> tile_resource_cumulative_probabilities = {
     0.10,  // POOR
@@ -54,16 +63,22 @@ const std::vector<double> tile_resource_cumulative_probabilities = {
     0.70,  // AVERAGE
     0.90,  // ABOVE_AVERAGE
     1.00   // GOOD
-};
+}; ///< Cumulative probabilities for each tile resource (to support procedural generation).
+
+// ======== END HEXTILE AND HEXMAP CONSTANTS ======================================== //
 
 
-//  4. ContextMenu constants
+
+// ======== CONTEXTMENU CONSTANTS =================================================== //
+
 const sf::Color MENU_FRAME_GREY(185, 187, 182); ///< The base colour of the context menu frame.
 const sf::Color MONOCHROME_SCREEN_BACKGROUND(40, 40, 40); ///< The base colour of old monochrome screens.
 const sf::Color VISUAL_SCREEN_FRAME_GREY(151, 151, 143); ///< The base colour of the framing of the visual screen.
 const sf::Color MONOCHROME_TEXT_GREEN(0, 255, 102); ///< The base colour of old monochrome text (green).
 const sf::Color MONOCHROME_TEXT_AMBER(255, 176, 0); ///< The base colour of old monochrome text (amber).
 const sf::Color MONOCHROME_TEXT_RED(255, 44, 0); ///< The base colour of old monochrome text (red).
+
+// ======== END CONTEXTMENU CONSTANTS =============================================== //
 
 
 #endif  /* CONSTANTS_H */
